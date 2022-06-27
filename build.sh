87cccaae55d128a82a54432894410b613fe5869f
Neil E. Pearson <neil.pearson@bigcommerce.com>
2022-06-27 16:27:34 +1000

    Fix build permissions issue

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ docker run \
   -v "$(pwd)":/project \
   -v "$(pwd)/out":/go \
   golang:1-alpine \
-  sh -c 'cd /project && go install'
+  sh -c "cd /project && go install && chown -R $(id -u "${USER}"):$(id -g "${USER}") /go/bin"
 mv out/bin/* ./
